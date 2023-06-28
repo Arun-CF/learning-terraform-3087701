b121ddc8458b185c04a7143d850acedc9778389a
Arun Chaudhary <133113704+Arun-CF@users.noreply.github.com>
2023-06-28 12:54:06 +0545

    Updating the code

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "New-test-SG"
   
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
   ingress_rules = ["http-80-tcp","https-443-tcp","all-icmp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
