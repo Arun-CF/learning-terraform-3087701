786045399e8aa8071f6f18092b65015129290094
Arun Chaudhary <133113704+Arun-CF@users.noreply.github.com>
2023-06-28 13:02:32 +0545

    Adding public_subnets[0]

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "New-test-SG"
   
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id.public_subnets[0]
   ingress_rules = ["http-80-tcp","https-443-tcp","all-icmp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
