a76e7588c66d8d07dbce20ea96ad96cc0c1ebdc4
Arun Chaudhary <133113704+Arun-CF@users.noreply.github.com>
2023-06-28 13:24:13 +0545

    Add sunetid to ec2 instance

--- main.tf
+++ main.tf
@@ -34,11 +34,13 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
   
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
 }
+
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
